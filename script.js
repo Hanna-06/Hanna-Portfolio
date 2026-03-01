67f4f2b57c0af51ad5e69826eacd7f51164cd72a
Hanna-06 <hannasusan06@gmail.com>
2026-03-01 22:56:49 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const cards = document.querySelectorAll(".card");
+
+window.addEventListener("scroll", revealCards);
+
+function revealCards() {
+    const triggerBottom = window.innerHeight * 0.85;
+
+    cards.forEach(card => {
+        const cardTop = card.getBoundingClientRect().top;
+
+        if (cardTop < triggerBottom) {
+            card.classList.add("visible");
+        }
+    });
+}
